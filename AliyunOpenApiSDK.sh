9e821cc3cc6551593d3c58873d73cf83a1127a9d
Zhong Lufan <lufanzhong@gmail.com>
2020-01-02 00:11:53 +0800

    Check if custom function exists

--- AliyunOpenApiSDK.sh
+++ AliyunOpenApiSDK.sh
@@ -60,8 +60,8 @@ aliapi_rpc() {
     for (( i = 0; i < ${#_ali_key[@]}; ++i )); do
         _key=${_ali_key[$i]}
         _value=${_ali_value[$i]}
-        # 参数值如果是以 () 结束，代表需要执行命令获取值。
-        [[ $(grep -E "^.+\(\)$" <<< "$_value")  == "$_value" ]] && _value=$(${_value//()/})
+        # 参数值如果是以 () 结束，代表需要执行函数获取值，如果函数不存在，使用原始值。
+        [[ ($(grep -E "^.+\(\)$" <<< "$_value")  == "$_value" && $(type -t ${_value//()/}) == "function") ]] && _value=$(${_value//()/})
         _value=$(_urlencode "$_value")
         _query_str+="$_key=$_value&"
     done
