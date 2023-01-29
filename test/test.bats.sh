abc588ed4089bc1186f1b8bcd45a9fe7f1e27355
Zhong Lufan <lufanzhong@gmail.com>
2023-01-29 22:32:03 +0800

    Add bats to git submodules

--- test/test.bats.sh
+++ test/test.bats.sh
@@ -32,8 +32,7 @@ test_timestamp_rpc() { #@test
     run _aliapi_timestamp_rpc
     [[ $status -eq 0 ]]
     timestamp=$output
-
-    run grep -P "^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])T(0\d|[1]\d|2[0-3]):[0-5]\d:[0-5]\d(Z|\+00:00)$" <<< "$timestamp"
+    run grep -E "^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])T(0[0-9]|[1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9](Z|\+00:00)$" <<< "$timestamp"
     [[ $status -eq 0 ]]
     [[ $output == "$timestamp" ]]
 }
